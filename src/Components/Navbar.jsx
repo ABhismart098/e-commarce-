e2021b5a4c4b822396395d52d7e42555bc75ae83
Abhishek Yadav <abhismart098@gmail.com>
2025-12-05 17:28:05 +0530

    fix the navbar issue in that issue not css file map with a project and add a footer also in this project

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,19 +1,20 @@
 import React from 'react'
+import { NavLink } from 'react-router'
 
 const Navbar = () => {
   return (
     <>
 
-<nav className="bg-neutral-primary fixed w-full z-20 top-0 start-0 border-b border-default">
+<nav className="bg-gray-900 sticky w-full z-20 top-0 start-0 border-b border-default">
   <div className="max-w-7xl flex flex-wrap items-center justify-between mx-auto p-4">
-  <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
-      <img src="https://flowbite.com/docs/images/logo.svg" className="h-7" alt="Flowbite Logo" />
-      <span className="self-center text-xl text-heading font-semibold whitespace-nowrap">Flowbite</span>
-  </a>
+  <NavLink href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
+      <img src="vite.svg" className="h-7" alt="Flowbite Logo" />
+      <span className="self-center dark: text-white text-xl text-heading font-semibold whitespace-nowrap">E-Commarce</span>
+  </NavLink>
   <div className="flex items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
       <button type="button" className="flex text-sm bg-neutral-primary rounded-full md:me-0 focus:ring-4 focus:ring-neutral-tertiary" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
         <span className="sr-only">Open user menu</span>
-        <img className="w-8 h-8 rounded-full" src="https://flowbite.com/docs/docs/images/people/profile-picture-5.jpg" alt="user photo"/>
+        <img className="w-8 h-8 rounded-full" src="https://flowbite.com/docs/images/people/profile-picture-5.jpg" alt="user photo"/>
       </button>
       {/* <!-- Dropdown menu --> */}
       <div className="z-50 hidden bg-neutral-primary-medium border border-default-medium rounded-base shadow-lg w-44" id="user-dropdown">
@@ -23,16 +24,20 @@ const Navbar = () => {
         </div>
         <ul className="p-2 text-sm text-body font-medium" aria-labelledby="user-menu-button">
           <li>
-            <a href="#" className="inline-flex items-center w-full p-2 hover:bg-neutral-tertiary-medium hover:text-heading rounded">Dashboard</a>
+            <NavLink
+             to="/" className="inline-flex items-center w-full p-2 hover:bg-neutral-tertiary-medium hover:text-heading rounded">Dashboard</NavLink>
           </li>
           <li>
-            <a href="#" className="inline-flex items-center w-full p-2 hover:bg-neutral-tertiary-medium hover:text-heading rounded">Settings</a>
+            <NavLink
+             to="/" className="inline-flex items-center w-full p-2 hover:bg-neutral-tertiary-medium hover:text-heading rounded">Settings</NavLink>
           </li>
           <li>
-            <a href="#" className="inline-flex items-center w-full p-2 hover:bg-neutral-tertiary-medium hover:text-heading rounded">Earnings</a>
+            <NavLink
+             to="/" className="inline-flex items-center w-full p-2 hover:bg-neutral-tertiary-medium hover:text-heading rounded">Earnings</NavLink>
           </li>
           <li>
-            <a href="#" className="inline-flex items-center w-full p-2 hover:bg-neutral-tertiary-medium hover:text-heading rounded">Sign out</a>
+            <NavLink
+             to="/" className="inline-flex items-center w-full p-2 hover:bg-neutral-tertiary-medium hover:text-heading rounded">Sign out</NavLink>
           </li>
         </ul>
       </div>
@@ -44,19 +49,19 @@ const Navbar = () => {
   <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-user">
     <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-default rounded-base bg-neutral-secondary-soft md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-neutral-primary">
       <li>
-        <a href="#" className="block py-2 px-3 text-white bg-brand rounded md:bg-transparent md:text-fg-brand md:p-0" aria-current="page">Home</a>
+        <NavLink to="/" className="block py-2 px-3 dark: text-white bg-brand rounded md:bg-transparent md:text-fg-brand md:p-0" aria-current="page">Home</NavLink>
       </li>
       <li>
-        <a href="#" className="block py-2 px-3 text-heading rounded hover:bg-neutral-tertiary md:hover:bg-transparent md:border-0 md:hover:text-fg-brand md:p-0 md:dark:hover:bg-transparent">About</a>
+        <NavLink to="/" className="block py-2 px-3 dark: text-white text-heading rounded hover:bg-neutral-tertiary md:hover:bg-transparent md:border-0 md:hover:text-fg-brand md:p-0 md:dark:hover:bg-transparent">About</NavLink>
       </li>
       <li>
-        <a href="#" className="block py-2 px-3 text-heading rounded hover:bg-neutral-tertiary md:hover:bg-transparent md:border-0 md:hover:text-fg-brand md:p-0 md:dark:hover:bg-transparent">Services</a>
+        <NavLink to="/" className="block py-2 px-3 dark: text-white text-heading rounded hover:bg-neutral-tertiary md:hover:bg-transparent md:border-0 md:hover:text-fg-brand md:p-0 md:dark:hover:bg-transparent">Services</NavLink>
       </li>
       <li>
-        <a href="#" className="block py-2 px-3 text-heading rounded hover:bg-neutral-tertiary md:hover:bg-transparent md:border-0 md:hover:text-fg-brand md:p-0 md:dark:hover:bg-transparent">Pricing</a>
+        <NavLink to="/" className="block py-2 px-3 dark: text-white text-heading rounded hover:bg-neutral-tertiary md:hover:bg-transparent md:border-0 md:hover:text-fg-brand md:p-0 md:dark:hover:bg-transparent">Pricing</NavLink>
       </li>
       <li>
-        <a href="#" className="block py-2 px-3 text-heading rounded hover:bg-neutral-tertiary md:hover:bg-transparent md:border-0 md:hover:text-fg-brand md:p-0 md:dark:hover:bg-transparent">Contact</a>
+        <NavLink to="/" className="block py-2 px-3 dark: text-white text-heading rounded hover:bg-neutral-tertiary md:hover:bg-transparent md:border-0 md:hover:text-fg-brand md:p-0 md:dark:hover:bg-transparent">Contact</NavLink>
       </li>
     </ul>
   </div>
